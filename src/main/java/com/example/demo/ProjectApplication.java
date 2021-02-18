12b3fd3f7b6a90783ccaec4dea0540dca14a51c8
pas-monah <paspaspas999@gmail.com>
2021-02-18 20:45:39 +0200

    ready for deploy

--- src/main/java/com/example/demo/ProjectApplication.java
+++ src/main/java/com/example/demo/ProjectApplication.java
@@ -17,29 +17,29 @@ public class ProjectApplication {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 	
-	@Bean
-	public ServletWebServerFactory webServer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint constraint = new SecurityConstraint();
-				constraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				constraint.addCollection(collection);
-				context.addConstraint(constraint);
-			}
-		};
-		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-		return tomcat;
-	}
-	
-	public Connector httpToHttpsRedirectConnector() {
-		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
-		return connector;
-	}
+//	@Bean
+//	public ServletWebServerFactory webServer() {
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//			@Override
+//			protected void postProcessContext(Context context) {
+//				SecurityConstraint constraint = new SecurityConstraint();
+//				constraint.setUserConstraint("CONFIDENTIAL");
+//				SecurityCollection collection = new SecurityCollection();
+//				collection.addPattern("/*");
+//				constraint.addCollection(collection);
+//				context.addConstraint(constraint);
+//			}
+//		};
+//		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+//		return tomcat;
+//	}
+//	
+//	public Connector httpToHttpsRedirectConnector() {
+//		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//		connector.setScheme("http");
+//		connector.setPort(8080);
+//		connector.setSecure(false);
+//		connector.setRedirectPort(8443);
+//		return connector;
+//	}
 }
